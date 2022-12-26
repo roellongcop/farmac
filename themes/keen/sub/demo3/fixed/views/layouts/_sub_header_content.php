14723b336fc8e7bb20fb1f1a3e2f6dd1ac18da7d
roellongcop <longcoproel@gmail.com>
2022-12-26 21:07:03 +0800

    c

---
+++ themes/keen/sub/demo3/fixed/views/layouts/_sub_header_content.php
@@ -0,0 +1,65 @@
+<?php
+
+use app\helpers\App;
+use app\helpers\Html;
+use app\helpers\Url;
+?>
+
+<!--begin::Logo-->
+<div class="d-none d-lg-flex align-items-center flex-wrap w-250px">
+	<!--begin::Logo-->
+	<a href="index.html">
+		<?= Html::image(App::setting('image')->primary_logo, ['w' => 150, 'quality' => 90], [
+            'alt' => 'Primary Logo',
+            'class' => 'max-h-50px',
+        ]) ?>
+	</a>
+	<!--end::Logo-->
+</div>
+<!--end::Logo-->
+<!--begin::Nav-->
+<div class="subheader-nav nav flex-grow-1">
+	<!--begin::Item-->
+	<a href="#" class="nav-item">
+		<span class="nav-label px-10">
+			<span class="nav-title text-dark-75 font-weight-bold font-size-h4">Diagnostic AI</span>
+			<span class="nav-desc text-muted">Ask the system</span>
+		</span>
+	</a>
+	<!--end::Item-->
+	<!--begin::Item-->
+	<!-- active -->
+	<a href="<?= Url::toRoute(['/announcement/index']) ?>" class="nav-item "> 
+		<span class="nav-label px-10">
+			<span class="nav-title text-dark-75 font-weight-bold font-size-h4">Announcements</span>
+			<span class="nav-desc text-muted">Publication Statements</span>
+		</span>
+	</a>
+	<!--end::Item-->
+	<!--begin::Item-->
+	<a href="<?= Url::toRoute(['/event/index']) ?>" class="nav-item "> 
+		<span class="nav-label px-10">
+			<span class="nav-title text-dark-75 font-weight-bold font-size-h4">Events</span>
+			<span class="nav-desc text-muted">Happenings & Calendar</span>
+		</span>
+	</a>
+	<!--end::Item-->
+	<!--begin::Item-->
+	<a href="<?= Url::toRoute(['/chat']) ?>" class="nav-item "> 
+		<span class="nav-label px-10">
+			<span class="nav-title text-dark-75 font-weight-bold font-size-h4">Community Board</span>
+			<span class="nav-desc text-muted">Group Chats | Public & Private</span>
+		</span>
+	</a>
+	<!--end::Item-->
+
+	<!--begin::Item-->
+	<a href="<?= Url::toRoute(['/user/my-account']) ?>" class="nav-item "> 
+		<span class="nav-label px-10">
+			<span class="nav-title text-dark-75 font-weight-bold font-size-h4">My Account</span>
+			<span class="nav-desc text-muted">Profile Information</span>
+		</span>
+	</a>
+	<!--end::Item-->
+</div>
+<!--end::Nav-->
