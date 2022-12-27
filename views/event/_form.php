1400a6c93380b203a58c27b543aada58c18eeb22
Roel Longcop <roel.filweb@gmail.com>
2022-12-27 09:51:52 +0800

    Update _form.php

--- views/event/_form.php
+++ views/event/_form.php
@@ -10,74 +10,75 @@ use app\widgets\Dropzone;
 /* @var $model app\models\Event */
 /* @var $form app\widgets\ActiveForm */
 ?>
-<?php $form = ActiveForm::begin(['id' => 'event-form']); ?>
+
     <div class="row">
         <div class="col-md-4">
             <?php $this->beginContent('@app/views/layouts/_card_wrapper.php', [
                 'title' => 'Event Form',
                 'stretch' => true
             ]) ?>
-                <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
-    			<?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+                <?php $form = ActiveForm::begin(['id' => 'event-form']); ?>
+                    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+        			<?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <?= DateTimePicker::widget([
-                            'form' => $form,
+                    <div class="row">
+                        <div class="col-md-6">
+                            <?= DateTimePicker::widget([
+                                'form' => $form,
+                                'model' => $model,
+                                'attribute' => 'start',
+                            ]) ?>
+                        </div>
+                        <div class="col-md-6">
+                            <?= DateTimePicker::widget([
+                                'form' => $form,
+                                'model' => $model,
+                                'attribute' => 'end',
+                            ]) ?>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Color</label>
+                        <?= $model->color ?>
+                        <div class="radio-inline">
+                            <label class="radio">
+                                <?= Html::input('radio', 'Event[color]', 'info', [
+                                    'checked' => $model->color == 'info'
+                                ]) ?>
+                                <span></span> Purple
+                            </label>
+                            <label class="radio">
+                                <?= Html::input('radio', 'Event[color]', 'success', [
+                                    'checked' => $model->color == 'success'
+                                ]) ?>
+                                <span></span> Green
+                            </label>
+                            <label class="radio">
+                                <?= Html::input('radio', 'Event[color]', 'danger', [
+                                    'checked' => $model->color == 'danger'
+                                ]) ?>
+                                <span></span> Red
+                            </label>
+                        </div>
+                    </div>
+                   
+                    <div class="text-center mt-10">
+                        <?= Dropzone::widget([
+                            'maxFiles' => 1,
+                            'tag' => 'Event',
+                            'files' => $model->imageFiles,
                             'model' => $model,
-                            'attribute' => 'start',
+                            'attribute' => 'photo',
+                            'acceptedFiles' => File::imageExtensions()
                         ]) ?>
                     </div>
-                    <div class="col-md-6">
-                        <?= DateTimePicker::widget([
-                            'form' => $form,
-                            'model' => $model,
-                            'attribute' => 'end',
-                        ]) ?>
+
+
+                    <div class="form-group mt-20">
+                        <?= ActiveForm::buttons() ?>
                     </div>
-                </div>
-
-                <div class="form-group">
-                    <label>Color</label>
-                    <?= $model->color ?>
-                    <div class="radio-inline">
-                        <label class="radio">
-                            <?= Html::input('radio', 'Event[color]', 'info', [
-                                'checked' => $model->color == 'info'
-                            ]) ?>
-                            <span></span> Purple
-                        </label>
-                        <label class="radio">
-                            <?= Html::input('radio', 'Event[color]', 'success', [
-                                'checked' => $model->color == 'success'
-                            ]) ?>
-                            <span></span> Green
-                        </label>
-                        <label class="radio">
-                            <?= Html::input('radio', 'Event[color]', 'danger', [
-                                'checked' => $model->color == 'danger'
-                            ]) ?>
-                            <span></span> Red
-                        </label>
-                    </div>
-                </div>
-               
-                <div class="text-center mt-10">
-                    <?= Dropzone::widget([
-                        'maxFiles' => 1,
-                        'tag' => 'Event',
-                        'files' => $model->imageFiles,
-                        'model' => $model,
-                        'attribute' => 'photo',
-                        'acceptedFiles' => File::imageExtensions()
-                    ]) ?>
-                </div>
-
-
-                <div class="form-group mt-20">
-                    <?= ActiveForm::buttons() ?>
-                </div>
-
+                <?php ActiveForm::end(); ?>
             <?php $this->endContent() ?>
         </div>
         <div class="col-md-8">
@@ -88,5 +89,5 @@ use app\widgets\Dropzone;
             <?php $this->endContent() ?>
         </div>
     </div>
-<?php ActiveForm::end(); ?>
+
 
