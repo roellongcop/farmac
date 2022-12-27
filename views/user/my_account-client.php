2f8ff891587001b096ec81661f3c65ca4a47eb60
roellongcop <longcoproel@gmail.com>
2022-12-27 21:10:06 +0800

    Update my_account-client.php

--- views/user/my_account-client.php
+++ views/user/my_account-client.php
@@ -20,7 +20,7 @@ $this->addJsFile('js/my-profile');
 
 
 <div class="modal fade" id="modal-add-documents" tabindex="-1" role="dialog" aria-labelledby="modal-add-documentsLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modal-add-documentsLabel">Add Documents</h5>
