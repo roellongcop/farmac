86a68084b5c02510032fa2419f2fe59866aa217e
Roel Longcop <roel.filweb@gmail.com>
2022-12-28 13:22:16 +0800

    Update _topbar-search-form.php

--- themes/keen/sub/demo3/fixed/views/layouts/_topbar-search-form.php
+++ themes/keen/sub/demo3/fixed/views/layouts/_topbar-search-form.php
@@ -3,12 +3,15 @@
 use app\widgets\ActiveForm;
 use app\widgets\Search;
 ?>
-<?php $form = ActiveForm::begin(['action' => $searchAction, 'method' => 'get']); ?>
-    <?= Search::widget([
-        'submitOnclick' => true,
-        'model' => $searchModel,
-        'options' => [
-            'style' => 'margin-top: 20px;'
-        ]
-    ]) ?>
-<?php ActiveForm::end(); ?>
+
+<div style="width: 25vw;">
+    <?php $form = ActiveForm::begin(['action' => $searchAction, 'method' => 'get']); ?>
+        <?= Search::widget([
+            'submitOnclick' => true,
+            'model' => $searchModel,
+            'options' => [
+                'style' => 'margin-top: 20px;'
+            ]
+        ]) ?>
+    <?php ActiveForm::end(); ?>
+</div>
