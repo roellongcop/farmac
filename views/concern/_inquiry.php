d164839faf9f4f4f8cc381613e56fd99a277887c
roellongcop <longcoproel@gmail.com>
2023-03-25 22:00:38 +0800

    c

--- views/concern/_inquiry.php
+++ views/concern/_inquiry.php
@@ -4,7 +4,7 @@ use app\models\Helpdesk;
 ?>
 
 <div class="navi-item">
-    <a href="#" class="navi-link btn-hidden-message" data-message="<?= $inquiry->name ?>" data-hidden_message="/concern-<?= $inquiry->concern_id ? $inquiry->concern_id: (Helpdesk::UNSOLVED_PATTERN . <?= $inquiry->name) ?> ?>">
+    <a href="#" class="navi-link btn-hidden-message" data-message="<?= $inquiry->name ?>" data-hidden_message="/concern-<?= $inquiry->concern_id ? $inquiry->concern_id: (Helpdesk::UNSOLVED_PATTERN . $inquiry->name) ?>">
         <span class="navi-text"><?= $counter ?>) <?= $inquiry->name ?>
         </span>
     </a>
